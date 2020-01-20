579f165c5d1d5ef224387f79b68965f6d085bdcf
dharma <juffyeh@gmail.com>
2020-01-20 17:20:09 +0800

    click action

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-$('#buttons > span').on('click',function(){
-    
-})
+var allButtons = $('#buttons > span')
+for(let i = 0;i < allButtons.length; i++){
+    $(allButtons[i]).on('click',function(e){
+        let index = $(e.currentTarget).index()
+        let s = index * -640
+        $('#images').css({
+            transform: 'translate(' + s + 'px)'
+        })
+        n = index
+        allButtons.eq(n).addClass('active').siblings('.active').removeClass('active')
+    })
+}
